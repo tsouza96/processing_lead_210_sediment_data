9c3541d4e6029206eb07e5b7288606ce0f404fdd
Joshua Himmelstein <joshua.himmelstein@gmail.com>
2023-02-22 14:32:23 -0500

    update selectpeaks.m
    autodetects peaks, no need for pre-defining ranges. This new selectpeaks automates peak counting and accomodates for shift in detector peak ranges through time.

--- selectpeaks.m
+++ selectpeaks.m
@@ -1,16 +1,16 @@
 %
 % File:   selectpeaks.m
-% Date:   2022-10-13
+% Date:   2023-02-22
 % Author: J. Himmelstein <himmelstein@unc.edu>
 %
 % Matlab function to extract counts for Po-209 and Po-210 based on known keV
 % ranges of interest/data in CHN files. Potential for drift of peaks, thus
 % re-calibration should be manually performed by plotting individual CHN
 % files and re-delineating peaks
-% 
+%
 % Usage: counts = selectpeaks(filetype, plotflag)
 %
-% Where:  
+% Where:
 %
 % filetype - required input: "chn" or "spe" - based on your filetype (.chn is
 % maestro native filetype, .spe is an ASCII file format)
@@ -18,8 +18,7 @@
 %
 % User prompted inputs will be required, follow formatting found on example
 % files in repository [e.g. 2021_Detector-Background.txt with MCAnum
-% Po-209	Po-209_err	Po-210	Po-210_err	Po-209_start	Po-209_end
-% Po-210_start	Po-210_end as headers and 2022-09-27_WC1_Core-Data.txt with
+% Po-209	Po-209_err	Po-210	Po-210_err as headers and 2022-09-27_WC1_Core-Data.txt with
 % Depth (cm) and Fine mass added to Tube (g) as column headers]
 %
 % counts is a structure with length = # of samples and the following fields:
@@ -27,7 +26,7 @@
 % depth             - vector of depth
 % counts_209_210    - raw counts within ROI(defined in det_bkgrnd_file)
 % cpm_209_210       - Net counts per minute [209 210]
-% Po_210_dpm_g      - Po-210 dpm/g 
+% Po_210_dpm_g      - Po-210 dpm/g
 % name              - Name of file / depth interval
 % detector          - Detector used for sampling and ROI definition
 % all               - Structure with all data produced by readchn function
@@ -38,7 +37,7 @@
 % and because no plotflag speficifed, will not auto-plot data (can be plotted later)
 %
 % or
-% 
+%
 % data = selectpeaks("chn",1) % if your files from the detector are saved
 % as .chn and will plot Pb-210 Profile at end of running
 
@@ -67,14 +66,21 @@ elseif filetype == "spe"
 end
 
 file_table = struct2table(file_list);
-fullFileNames = char(file_table.folder)+"\"+char(file_table.name);
-numFiles = length(fullFileNames);
+fullFileNames = append(file_table.folder,'\',file_table.name);
+fullFileNames = string(fullFileNames);
+numFiles = height(fullFileNames);
 
-disp("Add Detector Background File (with regions of interest for each peak included)")
-det_bkgrnd_file = uigetfile('/*.TXT;*.TSV'); %add detector background file
+disp("Add Detector Background File")
+det_bkgrnd_file = uigetfile('/*.TXT;*.CSV;*.TSV'); %add detector background file
+% det_bkgrnd_file = 'C:\Users\Rodriguez Lab\Desktop\Josh-Desktop\Marsh_Levee_Project\Wards_Creek\2011_Detector-Backgrounds.txt';
 det_bkgrnds = readtable(det_bkgrnd_file);
+
 disp("Add Core Data File [with Depth(cm), Sediment added to vial (g)]")
 core_data_file = uigetfile('/*.TXT;*.TSV');
+
+% folder = 'C:\Users\Rodriguez Lab\Desktop\Josh-Desktop\Marsh_Levee_Project\Wards_Creek';
+% core_data_file = fullfile(folder, '2022-09-27_WC-1_Core-Data.csv');
+
 core_data = readtable(core_data_file);
 
 % Loop over all files in d reading them in
@@ -95,27 +101,57 @@ for i = 1 : numFiles
 %     zoom on;
 %     movegui('west');
 
+
     det_idx= find(ismember(det_bkgrnds.MCAnum,(mcadat.mcanum),'rows'));
-    idx_209 = (mcadat.keV>det_bkgrnds.Po_209_start(det_idx) & mcadat.keV<det_bkgrnds.Po_209_end(det_idx));
-    idx_210 = (mcadat.keV>det_bkgrnds.Po_210_start(det_idx) & mcadat.keV<det_bkgrnds.Po_210_end(det_idx));
 
-    counts_209_210 = [sum(mcadat.count(idx_209)),sum(mcadat.count(idx_210))];
+    % Find the index of the first element greater than 5 (Start of 209
+    % Peak)
+    index1 = find(mcadat.count > 15, 1, 'first');
 
-    data.depth = core_data.Depth_cm_(i);
-    data.counts_209_210 = counts_209_210;
-    data.cpm_209_210 = (counts_209_210/mcadat.livetime)*60 - [det_bkgrnds.Po_209(det_idx) det_bkgrnds.Po_210(det_idx)];
-    data.cpm_error_209_210 = [sqrt((data.cpm_209_210(1)/mcadat.livetime/60)+(det_bkgrnds.Po_209_err(det_idx)/mcadat.livetime/60)+(det_bkgrnds.Po_209_err(det_idx)/mcadat.livetime/60)) sqrt((data.cpm_209_210(2)/mcadat.livetime/60)+(det_bkgrnds.Po_210_err(det_idx)/mcadat.livetime/60)+(det_bkgrnds.Po_210_err(det_idx)/mcadat.livetime/60))];
-    %     Inputs to (Po210/Po209)*(tracer_activity_dpm*pipette_mL)/(samplemass_mg/1000)
-    data.Po_210_dpm_g = ((data.cpm_209_210(2)/data.cpm_209_210(1))*(10*1))/(core_data.FineMassAddedToTube_g_(i)); 
-    data.cpm_error_209_210 = [det_bkgrnds.Po_209_err(det_idx) det_bkgrnds.Po_210_err(det_idx)];
-    %     Inputs to error: sqrt(((Po_209_net_cpm/Po_210_net_cpm)*sqrt((([Po_210_net_cpm_error/Po_210_net_cpm])^2)+(([Po_09_net_cpm_error/Po_209_net_cpm])^2))^2/[Po-210_dpm/g_]^2)+(([tracer_activity_dpm_error^2)/([tracer_activity_dpm]^2))+(([pipette(mL)error]^2)/([pipette(mL)]^2]))+(((.005*[sample_mass_mg_error])^2)/([sample_mass_mg(i)]^2)))
-    data.Po_210_dpm_g_error = sqrt(((data.cpm_209_210(1)/data.cpm_209_210(2))*sqrt(((data.cpm_error_209_210(2)/data.cpm_209_210(2))^2)+((data.cpm_error_209_210(1)/data.cpm_209_210(1))^2))^2/data.Po_210_dpm_g(1)^2)+((.719859^2)/(10^2))+((.001812^2)/(1^2))+(((.005*core_data.FineMassAddedToTube_g_(i))^2)/((core_data.FineMassAddedToTube_g_(i))^2)));
-    %     Variables: data.yield = =((Po_209_net_cpm)/(det_bkgrnds.total_alpha_3_8(det_idx)))/(tracer_activity*pipette_vol))*100);
-    data.yield = (((data.cpm_209_210(1)/(det_bkgrnds.total_alpha_3_8(det_idx)))/(10*1))*100);
-    data.name = extractAfter((string(mcadat.filename)),106); % change character # to extract after depending on your filename length
-    data.detector = mcadat.detector;
-    data.all = mcadat;
+    % Find the index of the first element less than 1 after the first
+    % element greater than 5 (End of 209 Peak)
+    index2 = find(mcadat.count(index1+1:end) < 1, 1, 'first') + index1;
+
+    % Find the index of the first element greater than 5 after index 2
+    % (Start of 210 peak)
+    index3 = find(mcadat.count(index2:end) > 15, 1, 'first') + index2;
+
+    % Find the index of the first element less than 1 after the third index
+    % found above (End of 210 Peak)
+    index4 = find(mcadat.count(index3:end) < 1, 1, 'first') + index3;
+
+    counts_209_210 = [sum(mcadat.count(index1:index2)),sum(mcadat.count(index3:index4))];
+
+    % Assign variables to data structure
+    data.depth = core_data.Depth_cm_(i); % depth data (must be at least as many core depths as samples processed, else error)
+    data.counts_209_210 = counts_209_210; % 209 and 210 counts
+    data.detector = mcadat.detector; % detector data
+
+    % Calculate CPM and CPM error for 209/210
+    cpm_209_210 = (counts_209_210/mcadat.livetime)*60 - [det_bkgrnds.Po_209(det_idx) det_bkgrnds.Po_210(det_idx)];
+    cpm_error_209_210 = [sqrt((cpm_209_210(1)/mcadat.livetime/60)+(det_bkgrnds.Po_209_err(det_idx)/mcadat.livetime/60)+(det_bkgrnds.Po_209_err(det_idx)/mcadat.livetime/60)) sqrt((cpm_209_210(2)/mcadat.livetime/60)+(det_bkgrnds.Po_210_err(det_idx)/mcadat.livetime/60)+(det_bkgrnds.Po_210_err(det_idx)/mcadat.livetime/60))];
+    data.cpm_209_210 = cpm_209_210; % assign cpm to data structure
+    data.cpm_error_209_210 = cpm_error_209_210; % assign cpm error to data structure
+
+    % Calculate Po-210 DPM/g
+    Po_210_dpm_g = ((cpm_209_210(2)/cpm_209_210(1))*(10*1))/(core_data.FineMassAddedToTube_g_(i));
+    data.Po_210_dpm_g = Po_210_dpm_g; % assign Po-210 DPM/g to data structure
+
+    % Calculate Po-210 DPM/g error
+    Po_210_dpm_g_error = sqrt(((cpm_209_210(1)/cpm_209_210(2))*sqrt(((cpm_error_209_210(2)/cpm_209_210(2))^2)+((cpm_error_209_210(1)/cpm_209_210(1))^2))^2/Po_210_dpm_g(1)^2)+((.719859^2)/(10^2))+((.001812^2)/(1^2))+(((.005*core_data.FineMassAddedToTube_g_(i))^2)/((core_data.FineMassAddedToTube_g_(i))^2)));
+    data.Po_210_dpm_g_error = Po_210_dpm_g_error; % assign Po-210 DPM/g error to data structure
+
+    % Calculate yield
+    yield = (((cpm_209_210(1)/(det_bkgrnds.total_alpha_3_8(det_idx)))/(10*1))*100);
+    data.yield = yield; % assign yield to data structure
+
+    % Assign filename to data structure
+    [filepath,name,ext] = fileparts(mcadat.filename);
+    data.name = name;
     
+    % Assign all data from mcadat to data structure
+    data.all = mcadat;
+   
     % for given iteration add data to count structure
     counts(i) = data;
 end
@@ -131,8 +167,8 @@ end
 
 % % Uncomment to write your data to a table that can be used in age-depth modeling, otherwise work
 % % with matlab structure created from this function
-% outputdata = table([counts.depth]', [counts.Po_210_dpm_g]', [counts.Po_210_dpm_g_error]');
-% outputdata.Properties.VariableNames = ["depth", "pbtotal", "upb"];
-% writetable(outputdata,'counts.csv')
+outputdata = table([counts.depth]', [counts.Po_210_dpm_g]', [counts.Po_210_dpm_g_error]');
+outputdata.Properties.VariableNames = ["depth", "pbtotal", "upb"];
+writetable(outputdata,'counts.csv')
 
 
